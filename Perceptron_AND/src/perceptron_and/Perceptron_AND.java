4982571e717e7dee5080ddd27bc814c117193e31
IZacarias <iuli.zacarias@gmail.com>
2014-05-12 16:40:21 -0300

    Adicionado links com referencias a paginas

---
+++ Perceptron_AND/src/perceptron_and/Perceptron_AND.java
@@ -0,0 +1,20 @@
+package perceptron_and;
+
+/**
+ *
+ * @author Iulisloi Zacarias
+ * http://www.generation5.org/content/2004/simple_ocr.asp
+ * http://www.generation5.org/content/1999/perceptron.asp
+ * http://en.literateprograms.org/Perceptron_%28Java%29
+ * http://www.cms.waikato.ac.nz/~abifet/MOA/API/_perceptron_8java_source.html
+ */
+public class Perceptron_AND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
